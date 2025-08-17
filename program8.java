1f0003cb50a6b0f633cbb6b1bc8916d90aaf0aa0
Vishal Kumar Singh <166538200+vishukumar005@users.noreply.github.com>
2025-08-17 15:41:58 +0530

    Add files via upload

---
+++ program8.java
@@ -0,0 +1,21 @@
+ import java.util.Scanner;
+ class Program8 {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int arr[] = new  int[] {1, 2, 3, 4, 5};
+     int evenSum = 0, i; 
+
+    System.out.print("Array elements: ");
+    for( i  = 0; i<arr.length; i++) {
+      System.out.print(arr[i]+" ");
+    }
+    for(i = 0; i<arr.length; i++) {
+      if(arr[i] % 2 == 0) {
+        evenSum += arr[i];
+      }
+    }
+    System.out.println("\nAll even number Sum: "+evenSum);
+    
+    sc.close();
+  }
+}
